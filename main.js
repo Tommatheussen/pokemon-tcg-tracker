a0474a784b42f5e0151d0a0980409b39725e52f6
Tom Matheussen <tmatheussen@avia-gis.com>
2017-05-23 12:22:17 +0200

    Fixed building process

--- main.js
+++ main.js
@@ -16,10 +16,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/build_client/index.html');
   //mainWindow.loadURL('http://localhost:4200');
   // Open the DevTools.
-   //mainWindow.webContents.openDevTools()
+
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
