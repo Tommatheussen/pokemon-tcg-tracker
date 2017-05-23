04c0dadfcaa247a5835ac9cdc9bbbbc3a84cac56
Tom Matheussen <tmatheussen@avia-gis.com>
2017-05-23 16:16:57 +0200

    Added icons

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
