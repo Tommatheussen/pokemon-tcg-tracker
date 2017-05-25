8059db705bd197ecb3970e885468fd193556ecd4
Tom Matheussen <tommatheussen5@hotmail.com>
2017-05-25 17:25:12 +0200

    Seed set list when requested

--- client/src/app/database/set.store.ts
+++ client/src/app/database/set.store.ts
@@ -10,83 +10,29 @@ import * as Datastore from 'nedb';
  * Performs CRUD on the NEDB data store that is passed to it in the constructor.
  */
 export class SetStore {
-    constructor(private db: Datastore, private http: Http) {
-        let count = db.count({}).exec((err, count) => {
-            if (count === 0) {
-                http.get('https://api.pokemontcg.io/v1/sets')
-                    .map((res: Response) => <Set[]>res.json().sets)
-					.subscribe(sets => db.insert(sets));
+  private setListSubject = new Rx.Subject();
 
-                //TODO: Seed
-                console.log('need to seed sets');
-            }
-        });
-    }
+  constructor(
+    private db: Datastore,
+    private http: Http) { }
 
-    /**
-     * List Pads
-     */
-    public getSets(): Observable<Set[]> {
-        return Rx.Observable.create(observer => {
-            this.db.find({}).exec((err, sets) => {
-                observer.next(sets);
-                observer.complete();
-            });
-        });
-    }
-
-    /*
-
-    public createPad(item: Pad): Observable<Pad> {
-        return Rx.Observable.create(observer => {
-            this.db.insert(item, () => {
-                observer.onNext(item);
-                observer.onCompleted();
-            });
-        });
-    }
-
-    public getPad(uuid: string): Observable<Pad> {
-        return Rx.Observable.create(observer => {
-            this.db.findOne({uuid: uuid}, (err, item) => {
-                observer.onNext(item);
-                observer.onCompleted();
-            });
-        });
-    }
-
-    public updatePad(pad: Pad): Observable<Pad> {
-        return Rx.Observable.create(observer => {
-            let updateObject = {};
-            for (let prop in pad) {
-                updateObject[prop] = pad[prop];
-            }
-            this.db.update({uuid: pad.uuid}, { $set: updateObject }, () => {
-                observer.onNext(true);
-                observer.onCompleted();
-            });
-        });
-    }
-
-    public deletePad(uuid: string): Observable<string> {
-        return Rx.Observable.create(observer => {
-            this.db.remove({uuid: uuid}, () => {
-                observer.onNext(uuid);
-                observer.onCompleted();
-            });
-        });
-    }
-
-    public addActionToPad(padUuid: string, action: Action): Observable<string> {
-        return Rx.Observable.create(observer => {
-            this.db.update({uuid: padUuid},
-                {
-                    $push: { history: action },
-                    $inc: { historyPointer: 1 }
-                }, () => {
-                    observer.onNext(padUuid);
-                    observer.onCompleted();
-                });
-        });
-    }*/
+  /**
+   * List Sets
+   */
+  public getSets(): Observable<Set[]> {
+    this.db.find({}).exec((err, dbsets) => {
+      if (dbsets.length === 0) {
+        this.http.get('https://api.pokemontcg.io/v1/sets')
+          .map((res: Response) => <Set[]>res.json().sets)
+          .subscribe(sets => {
+            this.db.insert(sets)
+            this.setListSubject.next(sets)
+          });
+      } else {
+        this.setListSubject.next(dbsets);
+      }
+    });
+    
+    return this.setListSubject;
+  }
 }
