99120abb9f2657ff142ab06fdb5dc80cdc70f674
Tom Matheussen <tommatheussen5@hotmail.com>
2017-05-29 23:11:41 +0200

    Started adding About in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { UpdaterService } from './updater.service';
+import { ElectronService } from 'ngx-electron';
+
 @Component({
   selector: 'pokemon-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private updaterService: UpdaterService) { }
+  constructor(
+    private updaterService: UpdaterService,
+    private electronService: ElectronService
+  ) { 
+    this.electronService.ipcRenderer.on('about', () => {
+      console.log('about');
+    });
+  }
 }
