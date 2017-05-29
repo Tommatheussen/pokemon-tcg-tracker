e171fe5979763bb2daac204168697ed282ffb6b7
Tom Matheussen <tommatheussen5@hotmail.com>
2017-05-29 23:01:46 +0200

    Auto reloading of electron

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+var electron = require('electron-connect').server.create();
+
+gulp.task('serve', function () {
+
+  // Start browser process
+  electron.start();
+
+  // Restart browser process
+  gulp.watch(['main.js', "electron/*"], electron.restart);
+});
