a8fcdb2550e0dae5319d3ae7c75d26caa8e90db2
Tom Matheussen <tmatheussen@avia-gis.com>
2017-05-30 11:22:09 +0200

    Fixed Mac menu

--- electron/menu.js
+++ electron/menu.js
@@ -39,10 +39,6 @@ if (process.platform === 'darwin') {
   template.unshift({
     label: app.getName(),
     submenu: [
-      {role: 'about'},
-      {type: 'separator'},
-      {role: 'services', submenu: []},
-      {type: 'separator'},
       {role: 'hide'},
       {role: 'hideothers'},
       {role: 'unhide'},
@@ -50,27 +46,6 @@ if (process.platform === 'darwin') {
       {role: 'quit'}
     ]
   })
-
-  // Edit menu
-  template[1].submenu.push(
-    {type: 'separator'},
-    {
-      label: 'Speech',
-      submenu: [
-        {role: 'startspeaking'},
-        {role: 'stopspeaking'}
-      ]
-    }
-  )
-
-  // Window menu
-  template[3].submenu = [
-    {role: 'close'},
-    {role: 'minimize'},
-    {role: 'zoom'},
-    {type: 'separator'},
-    {role: 'front'}
-  ]
 }
 
 const menu = Menu.buildFromTemplate(template)
